750c8684a050de1f63b41507b77e2017e1675a91
shishirm18 <shishirbittu1997@gmail.com>
2026-02-06 12:53:43 -0500

    updated push methods for array.

--- app.js
+++ app.js
@@ -15,30 +15,28 @@ document.addEventListener("keypress", function(){
 
 // Step 2 : Show its level 1 and start the btn flash
 let levelDisplay = document.querySelector('h3');
-let btns = document.querySelectorAll('.box');
+let btns = document.querySelectorAll('.box'); 
 
 for(btn of btns){
     btn.addEventListener("click", function(){
         flashBtn(this);
-        // userSeq.append(this.classList[1]);
+        userSeq.push(this.classList[1]);
     })
 }
 
-function startGame(){
+function gameOn(){
     level++;
     levelDisplay.innerText = `Level ${level}`;
 
-    // while(true){
-        //flash the random button for the user
-        let btn = document.querySelector(`.${randomBtn()}`);
-        flashBtn(btn);
+    
+    let allGood = true;
+    userSeq = []
 
-        gameSeq.append(randomBtn());
+    //flash the random button for the user
+    let btn = document.querySelector(`.${randomBtn()}`);
+    flashBtn(btn);
+    gameSeq.push(btn.classList[1]);
 
-        //Now its the user's turn to click on the same flashed btn
-        console.log("Game: ", gameSeq);
-        console.log("User: ", userSeq);
-    // }
 }
 
 //Helper: select the random button to flash
@@ -47,7 +45,7 @@ function randomBtn(){
     return `bc${randomNum}`;
 }
 
-// Helper function to flash the button
+// Helper: to flash the button
 function flashBtn(obj){
     obj.classList.add('flash');
     setTimeout(function() {
